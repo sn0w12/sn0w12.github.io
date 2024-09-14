cab7af17e09a90aa44fb39afa55ef71cbc910a8c
sn0w12 <lucasskold49@gmail.com>
2024-09-14 16:51:25 +0200

    Use several images

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,10 +1,13 @@
 window.onload = function() {
     // Fade in the main header
-    const content = document.querySelector('.content');
     const header = document.querySelector('.header');
-    const separator = document.querySelector('.separator');
+    const content = document.querySelector('.content');
+    const news = document.querySelector('.news-updates');
+    const separators = document.querySelectorAll('.separator');
     const subheaders = document.querySelectorAll('.subheader-link');
     const svg = document.getElementById('arathia-svg');
+
+    const footer = document.querySelector('.footer');
 
     //svg.style.height = `${textContent.offsetHeight}px`;
     content.style.opacity = '1';
@@ -17,7 +20,7 @@ window.onload = function() {
     }, startTime); // Stagger the fade-in with a delay
 
     // Fade in the subheader links one by one
-    separator.addEventListener('transitionend', () => {
+    separators[0].addEventListener('transitionend', () => {
         header.style.opacity = '1';
 
         subheaders.forEach((link, index) => {
@@ -31,4 +34,21 @@ window.onload = function() {
             svg.style.opacity = '1';
         }, (subheaders.length + 1) * subTime); // Trigger after the last subheader
     });
+
+    /*
+    svg.addEventListener('transitionend', () => {
+        setTimeout(() => {
+            footer.classList.add("animated");
+        }, startTime);
+    });
+
+    const footerPs = footer.querySelectorAll("p");
+    footer.addEventListener('transitionend', () => {
+        setTimeout(() => {
+            footerPs.forEach(text => {
+                text.style.opacity = '1';
+            });
+        }, subTime);
+    });
+    */
 };
