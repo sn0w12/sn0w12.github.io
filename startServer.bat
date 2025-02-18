ca649dc49c112118b1c912b04dcd5815eebbc692
Lucas  Sköld <lucasskold49@gmail.com>
2025-02-19 00:52:52 +0100

    Listen to local network

--- startServer.bat
+++ startServer.bat
@@ -1,5 +1,10 @@
 @echo off
 cd /d %~dp0
-start python -m http.server 8080
+echo Your computer's IP address(es):
+ipconfig | findstr /i "IPv4"
+start python -m http.server 8080 --bind 0.0.0.0
 start http://localhost:8080/
+echo.
+echo To access from other devices, use: http://YOUR_IP:8080
+echo (Use one of the IPv4 addresses shown above)
 pause
